79f410d9967d774d765e21553962c3eab35e413a
Rubel Ahmmed <rahmmed@redhat6.8>
2024-12-24 14:50:48 +0600

    Folder Management Rest Api Skelecton

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options  = {
+    arrowParens: "avoid",
+    tabWidth: 2,
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: "lf",
+    smi: false,
+    trailingComma: false
+}
+export default options;
